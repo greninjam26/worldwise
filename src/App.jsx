382322c8b327c1205c010eca210f9e81f5ed1b9f
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-15 22:03:49 -0400

    add in all the pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import "./index.css";
@@ -15,6 +16,7 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
+				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
