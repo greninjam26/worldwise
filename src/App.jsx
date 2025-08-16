85eced1c20c895baf5f6fa8cb05f951550655498
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-15 20:38:29 -0400

    Create all the routes with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  const i = 0;
-	return <div>WorldWise</div>;
+	return (
+		// this is only an additional to the JSX, there are more element outside the <BrowserRouter>, then they will always stay in the page
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
