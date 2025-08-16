3ad273f54076adb66b0cdb76364338dd7f9defb7
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-15 20:52:07 -0400

    connect between different pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
+import PageNav from "../components/PageNav";
+
 function HomePage() {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			<PageNav />
+			<h1>HomePage</h1>
+			<Link to="/pricing">Pricing</Link>
+		</div>
+	);
 }
 
 export default HomePage;
