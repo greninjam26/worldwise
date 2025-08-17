50d17951d16b2f32d619889ca7fc868098e62107
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-17 10:55:50 -0400

    Nested Routes and index Route

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,16 @@ function App() {
 		// this is only an additional to the JSX, there are more element outside the <BrowserRouter>, then they will always stay in the page
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<HomePage />} />
+				<Route index element={<HomePage />} />
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="login" element={<Login />} />
-				<Route path="app" element={<AppLayout />} />
+				<Route path="app" element={<AppLayout />}>
+					<Route index element={<p>List of cities</p>} />
+					<Route path="cities" element={<p>List of cities</p>} />
+					<Route path="countries" element={<p>Countries</p>} />
+					<Route path="form" element={<p>Form</p>} />
+				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
