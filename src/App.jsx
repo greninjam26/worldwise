b976d9858d0117cdc6abdcb110d9e0000aa5059b
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-17 18:23:34 -0400

    navigate between URLs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import "./index.css";
 
@@ -12,6 +12,7 @@ import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import City from "./components/City";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -45,10 +46,8 @@ function App() {
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route
-						index
-						element={<CityList cities={cities} isLoading={isLoading} />}
-					/>
+				{/* this is the alternative of navigating when useNavigate() will not work */}
+					<Route index element={<Navigate replace to="cities" />} />
 					<Route
 						path="cities"
 						element={<CityList cities={cities} isLoading={isLoading} />}
@@ -58,7 +57,7 @@ function App() {
 						path="countries"
 						element={<CountryList cities={cities} isLoading={isLoading} />}
 					/>
-					<Route path="form" element={<p>Form</p>} />
+					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
