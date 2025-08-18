9f0302b74a09b6ea592f96f6dbee5f170f0463ff
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-18 15:21:11 -0400

    delete Cities

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -59,6 +59,21 @@ function CitiesProvider({ children }) {
 		}
 	}
 
+	async function deleteCity(id) {
+		try {
+			setIsLoading(true);
+			await fetch(`${BASE_URL}/cities/${id}`, {
+				method: "DELETE",
+			});
+
+			setCities(cities => cities.filter(city => city.id !== id));
+		} catch (err) {
+			console.error(err);
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
 	return (
 		<CitiesContext.Provider
 			value={{
@@ -67,6 +82,7 @@ function CitiesProvider({ children }) {
 				currentCity,
 				fetchCity,
 				createCity,
+				deleteCity,
 			}}
 		>
 			{children}
