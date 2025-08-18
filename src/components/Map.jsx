f50d7b4a2672215f14a914575cfc310bc140aa93
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-18 09:59:56 -0400

    add city markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,9 +3,11 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import styles from "./Map.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
 	// const navigate = useNavigate();
+	const { cities } = useCities();
 	const [mapPosition, setMapPosition] = useState([40, 0]);
 
 	// const [searchParams, setSearchParams] = useSearchParams();
@@ -18,18 +20,20 @@ function Map() {
 			<MapContainer
 				center={mapPosition}
 				zoom={13}
-				scrollWheelZoom={false}
+				scrollWheelZoom={true}
 				className={styles.map}
 			>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
 				/>
-				<Marker position={mapPosition}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
+				{cities.map(city => (
+					<Marker position={city.position} key={city.id}>
+						<Popup>
+							<span>{city.emoji}</span> <span>{city.cityName}</span>
+						</Popup>
+					</Marker>
+				))}
 			</MapContainer>
 		</div>
 	);
