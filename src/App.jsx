db17a272d4db8abab7ef1b257c792e2ea5c981eb
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-18 20:36:57 -0400

    Protect the App

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import CityList from "./components/CityList";
 import City from "./components/City";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
+import ProtectedRoute from "./pages/ProtectedRoute";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 
@@ -26,7 +27,14 @@ function App() {
 						<Route path="product" element={<Product />} />
 						<Route path="pricing" element={<Pricing />} />
 						<Route path="login" element={<Login />} />
-						<Route path="app" element={<AppLayout />}>
+						<Route
+							path="app"
+							element={
+								<ProtectedRoute>
+									<AppLayout />
+								</ProtectedRoute>
+							}
+						>
 							{/* this is the alternative of navigating when useNavigate() will not work */}
 							<Route index element={<Navigate replace to="cities" />} />
 							<Route path="cities" element={<CityList />} />
