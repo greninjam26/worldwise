c2c07ff4d6924931a2f6909f28c09dc368ee42ad
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-18 20:11:12 -0400

    implement login

--- src/App.jsx
+++ src/App.jsx
@@ -13,29 +13,32 @@ import City from "./components/City";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
 	return (
-		<CitiesProvider>
-			{/* this is only an additional to the JSX, there are more element outside the <BrowserRouter>, then they will always stay in the page */}
-			<BrowserRouter>
-				<Routes>
-					<Route index element={<HomePage />} />
-					<Route path="product" element={<Product />} />
-					<Route path="pricing" element={<Pricing />} />
-					<Route path="login" element={<Login />} />
-					<Route path="app" element={<AppLayout />}>
-						{/* this is the alternative of navigating when useNavigate() will not work */}
-						<Route index element={<Navigate replace to="cities" />} />
-						<Route path="cities" element={<CityList />} />
-						<Route path="cities/:id" element={<City />} />
-						<Route path="countries" element={<CountryList />} />
-						<Route path="form" element={<Form />} />
-					</Route>
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</CitiesProvider>
+		<AuthProvider>
+			<CitiesProvider>
+				{/* this is only an additional to the JSX, there are more element outside the <BrowserRouter>, then they will always stay in the page */}
+				<BrowserRouter>
+					<Routes>
+						<Route index element={<HomePage />} />
+						<Route path="product" element={<Product />} />
+						<Route path="pricing" element={<Pricing />} />
+						<Route path="login" element={<Login />} />
+						<Route path="app" element={<AppLayout />}>
+							{/* this is the alternative of navigating when useNavigate() will not work */}
+							<Route index element={<Navigate replace to="cities" />} />
+							<Route path="cities" element={<CityList />} />
+							<Route path="cities/:id" element={<City />} />
+							<Route path="countries" element={<CountryList />} />
+							<Route path="form" element={<Form />} />
+						</Route>
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</CitiesProvider>
+		</AuthProvider>
 	);
 }
 
